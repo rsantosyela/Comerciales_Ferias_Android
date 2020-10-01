d3ddefb479dbaade40e70f06ed05ab8050be2fce
rsantosyela <rsantosyela@gmail.com>
2020-10-01 13:41:53 +0200

    List functionality added to My Contacts and Fairs activities

--- app/src/main/java/com/europesip/comerciales_ferias/FairsActivity.kt
+++ app/src/main/java/com/europesip/comerciales_ferias/FairsActivity.kt
@@ -1,23 +1,84 @@
 package com.europesip.comerciales_ferias
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 
 class FairsActivity : AppCompatActivity() {
+
+    // Variables
+    private lateinit var fairsListView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fairs)
+
+        // Initialized variables
+        fairsListView = findViewById(R.id.fairs_list_view)
 
         // Toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        //List view adapter
+        fairsListView.adapter = FairsCustomAdapter(this)
     }
 
     // Function so the toolbar button make a backpress
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    // Custom adapter needed to build list view contents
+    private class FairsCustomAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context = context
+
+        // Test arrays
+        private val fairNames = arrayListOf<String>("Fair 1", "Fair 2", "Fair 3", "Fair 4", "Fair 5", "Fair 6", "Fair 7")
+        private val fairLocations = arrayListOf<String>("Location 1", "Location 2", "Location 3", "Location 4", "Location 5", "Location 6", "Location 7")
+        private val fairDates = arrayListOf<String>("Date 1", "Date 2", "Date 3", "Date 4", "Date 5", "Date 6", "Date 7")
+
+        override fun getCount(): Int {
+            return fairNames.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        @SuppressLint("ViewHolder")
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val row = layoutInflater.inflate(R.layout.row_fairs, viewGroup, false)
+
+            // Fair names
+            val fairNameTextView = row.findViewById<TextView>(R.id.fair_name_text_view)
+            fairNameTextView.text = fairNames[position]
+
+            // Fair locations
+            val fairLocationTextView = row.findViewById<TextView>(R.id.fair_location_text_view)
+            fairLocationTextView.text = fairLocations[position]
+
+            // Fair dates
+            val fairDateTextView = row.findViewById<TextView>(R.id.fair_date_text_view)
+            fairDateTextView.text = fairDates[position]
+
+            return row
+        }
     }
 
 
--- app/src/main/java/com/europesip/comerciales_ferias/MyContacts.kt
+++ app/src/main/java/com/europesip/comerciales_ferias/MyContacts.kt
@@ -1,29 +1,88 @@
 package com.europesip.comerciales_ferias
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.os.Debug
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_menu.view.*
 
 class MyContacts : AppCompatActivity() {
+
+    // Variables
+    private lateinit var myContactsListView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_contacts)
+
+        // Initialized variables
+        myContactsListView = findViewById(R.id.my_contacts_list_view)
 
         // Toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
 
+        // List view adapter
+        myContactsListView.adapter = MyContactsCustomAdapter(this)
 
+        // Set on item click
+        myContactsListView.setOnItemClickListener { parent, view, position, id ->
 
+            // Item position
+            val itemPosition = position
 
+            // Test intent
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    // Function so the toolbar button make a backpress
+    // Function so the toolbar button makes a back press
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    // Custom adapter needed to build list view contents
+    private class MyContactsCustomAdapter(context: Context): BaseAdapter() {
+
+        private val mContext: Context = context
+        // Test array
+        private val contacts = arrayListOf<String>("Contact 1", "Contact 2", "Contact 3", "Contact 4", "Contact 5", "Contact 6", "Contact 7")
+
+        override fun getCount(): Int {
+            return contacts.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        @SuppressLint("ViewHolder")
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val row = layoutInflater.inflate(R.layout.row_my_contacts, viewGroup, false)
+
+            // Contact names
+            val contactNameTextView = row.findViewById<TextView>(R.id.contact_name_text_view)
+            contactNameTextView.text = contacts[position]
+
+            return row
+        }
     }
 }
